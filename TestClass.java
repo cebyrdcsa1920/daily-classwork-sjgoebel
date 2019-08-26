e41ec5ce0bbf3900d8c6f552ddef7e5059c296c5
sjgoebel <sgoebel@gmail.com>
2019-08-26 14:12:09 +0000

    Make week 3 folder and student class

--- TestClass.java
+++ TestClass.java
@@ -1,4 +1,30 @@
+/**
+ * This is a javadoc
+ *
+ */
+
 public class TestClass
 {
 
+    /**
+     * Program entry point
+     *
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+    /**
+     * Returns total sales from all vendors and
+     * sets totalSales to 0
+     *
+     * @return total amount of sales from all vendors
+     */
+    public int getTotalSales()
+    {
+        int sales = totalSales;
+        totalSales = 0;
+        return sales;
+    }
 }
